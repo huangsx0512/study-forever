4b7ed7cb82f5175214a7d52fc7f4221b5f46b184
huangsx0512 <huangsx0512@163.com>
2021-12-19 19:19:17 +0800

    新增DTO对象

---
+++ src/main/java/com/study/forever/dao/model/Grade.java
@@ -0,0 +1,13 @@
+package com.study.forever.dao.model;
+
+import lombok.Data;
+
+/**
+ * 年级
+ */
+@Data
+public class Grade {
+    private Integer gradeId;
+    private String gradeName;
+    private Integer studentId;
+}
--- src/main/java/com/study/forever/dao/model/Student.java
+++ src/main/java/com/study/forever/dao/model/Student.java
@@ -2,8 +2,12 @@ package com.study.forever.dao.model;
 
 import lombok.Data;
 
+/**
+ * 学生
+ */
 @Data
 public class Student {
+    private Integer studentId;
     private String name;
     private Integer age;
     private String grade;
